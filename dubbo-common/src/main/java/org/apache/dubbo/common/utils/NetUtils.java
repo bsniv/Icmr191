b06cb1f0724ede0184a31c4728bee617d2552de8
bsniv <bsniv10@gmail.com>
2019-01-16 18:23:20 +0200

    Refactoring - Replaced Temp With Query - Bucketing toAddress function in NetUtils

--- dubbo-common/src/main/java/org/apache/dubbo/common/utils/NetUtils.java
+++ dubbo-common/src/main/java/org/apache/dubbo/common/utils/NetUtils.java
@@ -318,25 +318,28 @@ public class NetUtils {
     }
 
     public static InetSocketAddress toAddress(String address) {
-        //Before
         int i = address.indexOf(':');
+        return new InetSocketAddress(getHostFromAddress(address, i), getPortFromAddress(address, i));
+    }
 
-        //Promoted
-        String host;
-        if (i > -1) {
-            host = address.substring(0, i);
-        } else {
-            host = address;
-        }
-
-        //After
+    private static int getPortFromAddress(String address, int i) {
         int port;
         if (i > -1) {
             port = Integer.parseInt(address.substring(i + 1));
         } else {
             port = 0;
         }
-        return new InetSocketAddress(host, port);
+        return port;
+    }
+
+    private static String getHostFromAddress(String address, int i) {
+        String host;
+        if (i > -1) {
+            host = address.substring(0, i);
+        } else {
+            host = address;
+        }
+        return host;
     }
 
     public static String toURL(String protocol, String host, int port, String path) {
